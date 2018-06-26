471d0d1648311d49f8be7194744fcbeb5f5a6f1d
Jake Smith <jake.smith@bigcommerce.com>
2018-06-26 15:45:23 -0500

    make it better

---
+++ config/keys_prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongoURI: "process.env.MONGO_URI",
+    secretOrkey: "process.env.SECRET",
+    bchash: "process.env.BCHASH",
+    bcclientId: "process.env.BCCLIENTID",
+    bcToken: "process.env.BCTOKEN",
+    bcSecret: "process.env.BCSECRET",
+    getBCemail: "process.env.GETBCEMAIL",
+    bcCustAPI: "process.env.BCCUSTAPI",
+    bcGCAPI: "process.env.BCGCAPI",
+    bcCartAPI: "process.env.BCCARTAPI"
+};
